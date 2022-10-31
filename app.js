d8a05f0951f14900033d011ea99f32cbabe99b42
uihalagedara <it20224820@my.sliit.lk>
2022-11-01 01:09:52 +0530

    passenger added to app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import multer from "multer";
 import "dotenv/config";
 import { connect } from "./utils/connection.js";
 
+import passengerRoutes from "./routes/passenger_routes.js";
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
@@ -14,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 connect();
 
+app.use("/ts", passengerRoutes);
 
 
 app.get("/", (req, res) => {
